29a0f5ce988fd61322f95875f6361585f039254e
Stephen Mather <stephen@smathermather.com>
2013-11-01 16:05:05 -0400

    Added a comment to explain the query

--- sql/municipalities.sql
+++ sql/municipalities.sql
@@ -1,3 +1,5 @@
+// Selects out all but Cleveland as a municipality, and unions those data with the Cleveland Statistical Planning Areas
+
 SELECT cartodb_id ,municipa_4 AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "municipa_4" <> 'Cleveland'
 
 UNION
