6a0549fe216f41430b010e2b201a11dafe2abff0
Stephen Mather <stephen@smathermather.com>
2013-11-05 13:06:17 -0500

    trtotpct to percent_canopy rename

--- sql/tracts.sql
+++ sql/tracts.sql
@@ -1,3 +1,3 @@
 // Query to pull out just tracts
 
-SELECT cartodb_id, E'Tract \n' || tl_2010__6 AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Tracts'
+SELECT cartodb_id, E'Tract \n' || tl_2010__6 AS name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Tracts'
