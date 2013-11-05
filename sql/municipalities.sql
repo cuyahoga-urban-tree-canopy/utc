fa8d48f98ae9d76d58036a61ad44d630640d2a98
Stephen Mather <stephen@smathermather.com>
2013-11-05 11:25:10 -0500

    Oh, adding a little label formatting right in the SQL

--- sql/municipalities.sql
+++ sql/municipalities.sql
@@ -1,7 +1,7 @@
 // Selects out all but Cleveland as a municipality, and unions those data with the Cleveland Statistical Planning Areas
 
-SELECT cartodb_id ,municipa_4 AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "municipa_4" <> 'Cleveland'
+SELECT cartodb_id , replace(municipa_4, ' ', E'\n') AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "municipa_4" <> 'Cleveland'
 
 UNION
 
-SELECT cartodb_id, spa_name AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'ClevelandSPA'
+SELECT cartodb_id, replace(spa_name, ' ', E'\n')  AS name, percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'ClevelandSPA'
