44cb817395e421c4dafb5c7075e8d0d0690295d9
Stephen Mather <stephen@smathermather.com>
2013-11-08 10:28:49 -0500

    Simplified underlying data to merge mun and SPAs-- treating SPAs as municipalities

--- sql/municipalities.sql
+++ sql/municipalities.sql
@@ -1,7 +1,3 @@
-// Selects out all but Cleveland as a municipality, and unions those data with the Cleveland Statistical Planning Areas
+// Selects just municipalities
 
-SELECT cartodb_id , replace(municipa_4, ' ', E'\n') AS name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "municipa_4" <> 'Cleveland'
-
-UNION
-
-SELECT cartodb_id, replace(spa_name, ' ', E'\n')  AS name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'SPA'
+SELECT cartodb_id , replace(name, ' ', E'\n') AS name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE type = 'Municipality';
