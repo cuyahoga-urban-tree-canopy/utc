1d65730d079de00c77f16af13209fea659600e78
Stephen Mather <stephen@smathermather.com>
2013-11-12 16:49:40 -0500

    Adding rounded version of percent canopy

--- sql/blocks.sql
+++ sql/blocks.sql
@@ -1,3 +1,3 @@
 // Select just that which we need from blocks data
 
-SELECT cartodb_id, name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Block'
+SELECT cartodb_id, E'Block \n' || name AS name, trtotpct AS percent_canopy, round(trtotpct::numeric, 0) || '%' AS "Percent Canopy", the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Block'
