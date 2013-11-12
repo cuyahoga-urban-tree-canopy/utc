8ad292c4f5c947ebb948249ecca21c2b80944e34
Stephen Mather <stephen@smathermather.com>
2013-11-12 15:01:06 -0500

    Add name column

--- sql/blocks.sql
+++ sql/blocks.sql
@@ -1,3 +1,3 @@
 // Select just that which we need from blocks data
 
-SELECT cartodb_id, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Block'
+SELECT cartodb_id, name, trtotpct AS percent_canopy, the_geom_webmercator FROM utc_summarized_all WHERE "type" = 'Block'
