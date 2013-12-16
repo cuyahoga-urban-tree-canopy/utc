8270f52a4881577d1355d1229fc9a4cff89a7c60
Stephen Mather <stephen@smathermather.com>
2013-12-16 14:49:14 -0500

    update to reflect new table name

--- sql/tracts.sql
+++ sql/tracts.sql
@@ -1,3 +1,4 @@
-// Query to pull out just tracts
-
-SELECT cartodb_id, E'Tract \n' || name AS name, trtotpct AS percent_canopy, round(trtotpct::numeric, 0) || '%' AS "Percent Canopy", the_geom_webmercator, type FROM utc_summarized_all WHERE "type" = 'Tract'
+SELECT cartodb_id, extcpctall AS percent_canopy, 
+	replace(name, ' ', E'\n') AS name,
+  	the_geom_webmercator
+    FROM tracts
