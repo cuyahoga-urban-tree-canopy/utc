93854212d174478b137a003421b9df10ac68cca2
Stephen Mather <stephen@smathermather.com>
2013-12-16 14:23:14 -0500

    rename column on the fly, apply some formatting

--- sql/watersheds.sql
+++ sql/watersheds.sql
@@ -1,1 +1,4 @@
-SELECT cartodb_id, extcpctall, (regexp_split_to_array(name, E'\\|'))[1] AS name, the_geom_webmercator FROM utc_watersheds
+SELECT cartodb_id, extcpctall AS percent_canopy,
+  (regexp_split_to_array(name, E'\\|'))[1] AS name,
+  the_geom_webmercator
+    FROM utc_watersheds
