576ddce8efa4d02f84d3cd8b72ae25137e7ff43e
Stephen Mather <stephen@smathermather.com>
2014-04-08 13:06:04 -0400

    update to include initial chart fields

--- sql/blocks.sql
+++ sql/blocks.sql
@@ -1,3 +1,9 @@
-SELECT cartodb_id, extcpctall AS percent_canopy, 
-        name, the_geom_webmercator
+SELECT cartodb_id,
+        extcpctall AS percent_canopy, 
+        round(extc_ac) AS "tree", round(exop_ac) AS "other",
+        round(exrd_ac) AS "rrr", round(exbl_ac) AS "build",
+        round(exwa_ac) AS "water", round(exbs_ac) AS "soil",
+        round(exgr_ac) AS "grass",
+        name,
+        the_geom_webmercator
     FROM blocks
